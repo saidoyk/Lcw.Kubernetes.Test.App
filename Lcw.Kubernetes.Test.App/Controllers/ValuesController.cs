11c91460fb4c048c40f520cb90bb88db1d2d426c
ERSAN GUNDOGDU <ERSAN.GUNDOGDU@lcwaikiki.com>
2023-06-15 09:54:25 +0300

    create project
    create project

---
+++ Lcw.Kubernetes.Test.App/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lcw.Kubernetes.Test.App.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string ping()
+        {
+            return "pong";
+        }
+
+    }
+}
